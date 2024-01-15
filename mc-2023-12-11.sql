2ee8f25e9c2025e47c1048ec4de90c42cf253ef3
Ania <85602903+anncym21@users.noreply.github.com>
2024-01-15 10:31:56 +0100

    ¯\_(■ᨓ■)_/¯

--- mc-2023-12-11.sql
+++
@@ -1,9 +0,0 @@
-DROP PROCEDURE IF EXISTS available_cars_in_price_range; 
-DELIMITER //
-CREATE PROCEDURE available_cars_in_price_range(IN min_value INT, IN max_value INT)
-BEGIN
-SELECT `id`, `rental_charge`, `brand`, `model`, `vehicle_type`, `vin`, `year_of_production`, `mileage`, `seats`, `department_id`, `insurance_id`
-FROM `vehicles_(pojazdy)` 
-WHERE `availability` = "AVAILABLE" AND `rental_charge` >= min_value AND `rental_charge` <= max_value
-ORDER BY `rental_charge`;
-END //
